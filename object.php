5fb93c13c6a8e9fe1e1999541ceb4c8f6a324743
Samuel Ström <samuelstrom93@gmail.com>
2020-10-22 13:58:01 +0200

    lite ändringar på morgonen

--- object.php
+++ object.php
@@ -6,27 +6,26 @@ $title = "Objekt";
 include("incl/header.php"); ?>
 
 <section>
-<h1>Objekt</h1>
+    <h1>Objekt</h1>
 
-<?php 
-$db = connectToDatabase($dsn);
-
-$result = getAllFrom($db, "object");?>
-
-
-<div class="object-flexbox">
-
-<?php foreach ($result as $row) {
-    ?>
-    <img src="img/150x150/<?=$row['image1']?>" alt="bild">
-    <p><?=$row['title']?></p>
     <?php
-}
-?>
-</div>
+    $db = connectToDatabase($dsn);
+    $result = getAllFrom($db, "object"); ?>
 
-<?php include("incl/footer.php");?>
+
+    <div class="flex-container">
+        <?php foreach ($result as $row) {
+        ?>
+            <div class="flex-wrap">
+                <div class="picture"><img src="img/150x150/<?= $row['image1'] ?>" alt="bild"></div>
+                <h3><?= $row['title'] ?></h3>
+                <!-- <p><?= $row['image1Text'] ?></p> -->
+            </div>
+        <?php
+        }
+        ?>
+    </div>
+
+    <?php include("incl/footer.php"); ?>
 
 </section>
-
-
